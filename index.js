7f7021892ea910d012678f951c765a0bd1c200f6
aleclarson <alec.stanford.larson@gmail.com>
2017-11-25 20:21:11 -0500

    refactor: replace \/ with /
    The `RegExp` constructor does proper substitution for us.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 function globRegex(glob) {
   const pattern = glob
     .replace('.', '\\.')
-    .replace('**/', '(.+\\/)?')
-    .replace('**', '(.+\\/)?*')
-    .replace('*', '[^\\/]+')
+    .replace('**/', '(.+/)?')
+    .replace('**', '(.+/)?*')
+    .replace('*', '[^/]+')
 
   return new RegExp('^' + pattern + '$')
 }
