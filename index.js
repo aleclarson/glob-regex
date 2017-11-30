52b6c65871774177ae88b4a4051803b664f4ff3a
aleclarson <alec.stanford.larson@gmail.com>
2017-11-30 18:51:13 -0500

    fix: use global regexp for glob replacement

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
+const dotRE = /\./g
+const globAllRE = /\*\*/g
+const globDirsRE = /\*\*\//g
+const globNamesRE = /\*/g
+
 function globRegex(glob) {
   const pattern = glob
-    .replace('.', '\\.')
-    .replace('**/', '(.+/)?')
-    .replace('**', '(.+/)?*')
-    .replace('*', '[^/]+')
+    .replace(dotRE, '\\.')
+    .replace(globDirsRE, '(.+/)?')
+    .replace(globAllRE, '(.+/)?*')
+    .replace(globNamesRE, '[^/]+')
 
   return new RegExp('^' + pattern + '$')
 }
