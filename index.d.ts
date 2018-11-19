c65f9711f6e5c9e6ee487dc2db07927aa18ed6e5
aleclarson <alec.stanford.larson@gmail.com>
2018-11-19 16:23:01 -0500

    ts: add `replace` function

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,6 @@
-export type GlobParser = (glob: string | string[]) => RegExp
+export interface GlobParser {
+  (glob: string | string[]): RegExp
+  replace(glob: string): string
+}
 declare const globRegex: GlobParser
 export default globRegex
